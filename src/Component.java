bd516c9487b2907ab2f8d31f601eafb80a8913e9
chinahehaiyang <imhhy123@hotmail.com>
2019-01-11 09:03:29 -0700

    finished writing functions in packedVector. Still need unit testing

--- src/Component.java
+++ src/Component.java
@@ -1,71 +1,118 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 
 class Component<T>
 {
 
-	public ArrayList<Integer> sparse_vector;
-	public ArrayList<Integer> packed_indices;
-	public ArrayList<T> packed_data;
-	public int next_free_index;
+	public ArrayList<Integer> m_sparse_vector;
+	public ArrayList<Integer> m_packed_indices;
+	public ArrayList<T> m_packed_data;
+	public int m_next_free_index;
 
 	public Component(int capacity)
 	{
-		// TODO only set the capacity, NOT the size!!??????
-		sparse_vector = new ArrayList<Integer>(
+		m_sparse_vector = new ArrayList<Integer>(
 			Collections.nCopies(capacity, -1));
-		packed_indices = new ArrayList<Integer>(capacity);
-		packed_data = new ArrayList<T>(capacity);
-		next_free_index = 0;
+		m_packed_indices = new ArrayList<Integer>(capacity);
+		m_packed_data = new ArrayList<T>(capacity);
+		m_next_free_index = 0;
 	}
 
 	public void add_element_at_sparse_vector(final int index, final T val)
 	{
-		if (index >= sparse_vector.size()) {
+		if (index >= m_sparse_vector.size()) {
 			System.out.println(
 				"MAJOR ERROR IN PACKEDVECTOR. Too many entities in this engine! increase buffer size.");
 			return;
-		} else if (sparse_vector.get(index) == -1) {
+		} else if (m_sparse_vector.get(index) == -1) {
 			System.out.println(
 				"MAJOR ERROR IN PACKEDVECTOR. You are adding an entity at this index, but an entity already exist at this index.");
 			return;
 		}
-		sparse_vector.set(index, next_free_index);
-		packed_indices.add(index);
-		packed_data.add(val);
+		m_sparse_vector.set(index, m_next_free_index);
+		m_packed_indices.add(index);
+		m_packed_data.add(val);
 
-		++next_free_index;
+		++m_next_free_index;
 	}
 
 	public void delete_element_at_sparse_vector(final int index)
 	{
-		if (sparse_vector.get(index) == -1) {
+		if (m_sparse_vector.get(index) == -1) {
 			System.out.println(
 				"MINOR error in packedvector. You are deleting an entity that had already been deleted. The program should continue to work normally.");
 			return;
 		}
-		int toBeDeletedIndexInPackedIndicies = sparse_vector.get(index);
+		int toBeDeletedIndexInPackedIndicies =
+			m_sparse_vector.get(index);
 		int lastElementInPackedIndicies =
-			packed_indices.get(packed_data.size() - 1);
+			m_packed_indices.get(m_packed_data.size() - 1);
 
 		Collections.swap(
-			sparse_vector, index,
-			sparse_vector.get(lastElementInPackedIndicies));
-		Collections.swap(packed_indices,
+			m_sparse_vector, index,
+			m_sparse_vector.get(lastElementInPackedIndicies));
+		Collections.swap(m_packed_indices,
 				 toBeDeletedIndexInPackedIndicies,
-				 packed_indices.size() - 1);
-		Collections.swap(packed_data, toBeDeletedIndexInPackedIndicies,
-				 packed_data.size() - 1);
+				 m_packed_indices.size() - 1);
+		Collections.swap(m_packed_data,
+				 toBeDeletedIndexInPackedIndicies,
+				 m_packed_data.size() - 1);
 
-		packed_data.remove(packed_data.size() - 1);
-		packed_indices.remove(packed_indices.size() - 1);
+		m_packed_data.remove(m_packed_data.size() - 1);
+		m_packed_indices.remove(m_packed_indices.size() - 1);
 
-		// TODO the value at sparsevector line up with the index of the
-		// index??
-		sparse_vector.set(index, -1);
-		// OR sparse_vector.set(toBeDeleted, -1); ??????????
-		--next_free_index;
+
+		m_sparse_vector.set(index, -1);
+		--m_next_free_index;
+	}
+
+	public final T get_data_from_sparse_vector(final int index)
+	{
+		if (index >= m_sparse_vector.size()) {
+
+			System.out.println(
+				"MAJOR ERROR IN PACKEDVECTOR. Index is bigger than the size of sparse vector with get_data_from_sparse_vector function");
+		} else if (m_sparse_vector.get(index) == -1) {
+			System.out.println(
+				"MAJOR ERROR IN PACKEDVECTOR. Accessing invalid sparse vector index with get_data_from_sparse_vector function");
+		}
+		return m_packed_data.get(m_sparse_vector.get(index));
+	}
+
+	public final int get_global_index_from_packed_index(final int index)
+	{
+
+		if (index >= m_packed_indices.size()) {
+
+			System.out.println(
+				"MAJOR ERROR IN PACKEDVECTOR. Index is bigger than the size of packed indices vector with get_global_index_from_packed_index function");
+		}
+		return m_packed_indices.get(index);
+	}
+
+	public final ArrayList<Integer> get_sparse_vector()
+	{
+		return m_sparse_vector;
+	}
+
+	public final ArrayList<Integer> get_packed_indicies()
+	{
+		return m_packed_indices;
+	}
+
+	public final ArrayList<T> get_packed_data()
+	{
+		return m_packed_data;
+	}
+
+	public final int get_packed_data_size()
+	{
+		if (m_packed_data.size() != m_packed_indices.size()) {
+
+			System.out.println(
+				"MAJOR ERROR IN PACKEDVECTOR. Packed indices and packed data are not the same size!");
+		}
+		return m_packed_data.size();
 	}
 }
