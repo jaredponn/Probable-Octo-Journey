41e8a61fe66ffa2f906b35999dc702d1a78891b3
jaredponn <jpon1234@gmail.com>
2019-01-11 15:11:47 -0700

    updates to the render component -- merely just testing

--- src/Components/Render.java
+++ src/Components/Render.java
@@ -1,5 +1,15 @@
 package Components;
 
-public class Render
+import poj.Component;
+
+public class Render extends Component
 {
+	private int m_a;
+	private int m_b;
+
+	public Render(int a, int b)
+	{
+		m_a = a;
+		m_b = b;
+	}
 }
