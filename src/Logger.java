352d7c664180e1de4ae682bd07eac79d3bb23049
jaredponn <jpon1234@gmail.com>
2019-01-11 08:54:12 -0700

    added logger

---
+++ src/Logger.java
@@ -0,0 +1,42 @@
+
+
+enum LOG_LEVEL {
+	ALL,		// logs everything (0)
+	VERBOSE,	// verbose error logging (1)
+	MINOR_CRITICAL, // minor critical errors (2)
+	MAJOR_CRITICAL, // major erros that crash the program (3)
+}
+
+class LOG_LEVEL_CAST
+{
+	public static int castToInt(LOG_LEVEL lvl)
+	{
+		int n = 0;
+
+		for (LOG_LEVEL i : LOG_LEVEL.values()) {
+			if (lvl == i)
+				break;
+			++n;
+		}
+
+		return n;
+	}
+}
+
+class Logger
+{
+	public static LOG_LEVEL m_logging_level = LOG_LEVEL.ALL;
+
+	public static final void logMessage(String str, LOG_LEVEL lvl)
+	{
+
+		if (LOG_LEVEL_CAST.castToInt(lvl)
+		    >= LOG_LEVEL_CAST.castToInt(m_logging_level)) {
+			System.out.println(str);
+		}
+
+		if (lvl == LOG_LEVEL.MAJOR_CRITICAL) {
+			System.exit(0);
+		}
+	}
+}
