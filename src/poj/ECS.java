cc1e00c60b3b26c21575a16dfc25c1808b4bb5d9
jaredponn <jpon1234@gmail.com>
2019-01-11 15:35:44 -0700

    ECS type

---
+++ src/poj/ECS.java
@@ -0,0 +1,15 @@
+package poj;
+
+import poj.Component;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ECS
+{
+	// State
+	private HashMap<Class<?>, Component<Class<?>>> m_component_list;
+	private Stack<Integer> m_free_indices;
+
+	// Functions
+	public registerComponent();
+}
