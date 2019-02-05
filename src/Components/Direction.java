887d6cb7bdce8f4f2d4600b05dc876cabb0ee878
jaredponn <jpon1234@gmail.com>
2019-02-04 21:26:36 -0700

    revamped directions to use the CardinalDirections enum

--- src/Components/Direction.java
+++ src/Components/Direction.java
@@ -3,68 +3,44 @@ package Components;
 import poj.Component.Component;
 import poj.linear.*;
 
-/*
-   TODO  -- not working
-*/
-
-enum Directions implements Component {
-	N {
-		public void print()
-		{
-			System.out.println("North");
-		}
-	},
-
-	E {
-		public void print()
-		{
-			System.out.println("East");
-		}
-	},
-
-	S {
-		public void print()
-		{
-			System.out.println("South");
-		}
-	},
-
-
-	W {
-		public void print()
-		{
-			System.out.println("West");
-		}
-	};
-}
-
-
 public class Direction implements Component
 {
-	private Directions direction;
+	private CardinalDirections direction;
 
-	public static final float UNIT_DIAGONAL = 0.70710678118f;
+	public Direction(CardinalDirections n)
+	{
+		setDirection(n);
+	}
+
+	public void setDirection(CardinalDirections n)
+	{
+		this.direction = n;
+	}
+
+	public void setDirectionFromVectorAndSnapToClosestDirection(Vector2f n)
+	{
+		this.direction =
+			CardinalDirections
+				.getClosestDirectionFromDirectionVector(n);
+	}
+
+	public CardinalDirections getDirection()
+	{
+		return this.direction;
+	}
+
+	public Vector2f getDirectionAsUnitVector()
+	{
+		return CardinalDirections.getUnitVector(this.direction);
+	}
+
+	public Vector2f getUnitVector()
+	{
+		return CardinalDirections.getUnitVector(this.direction);
+	}
 
 	public void print()
 	{
-	}
-
-	public static Vector2f getDirectionUnitVector(Direction d)
-	{
-		/*
-		switch (d) {
-		case N:
-			return new Vector2f(0.f, -1.f);
-		case E:
-			return new Vector2f(1.f, 0.f);
-		case W:
-			return new Vector2f(-1.f, 0.f);
-		case S:
-			return new Vector2f(0.f, 1.f);
-		default:
-			return new Vector2f(0, 0);
-		}*/
-
-		return new Vector2f(0, 0);
+		CardinalDirections.print(direction);
 	}
 }
