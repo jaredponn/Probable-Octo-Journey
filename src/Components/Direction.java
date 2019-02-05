41b5b14bdaf932585190c056f5569b3eed34c98e
jaredponn <jpon1234@gmail.com>
2019-02-04 19:50:22 -0700

    still working on this'

--- src/Components/Direction.java
+++ src/Components/Direction.java
@@ -1,34 +1,70 @@
 package Components;
 
 import poj.Component.Component;
+import poj.linear.*;
 
-public enum Direction implements Component {
+/*
+   TODO  -- not working
+*/
 
-	NORTH {
+enum Directions implements Component {
+	N {
 		public void print()
 		{
 			System.out.println("North");
 		}
 	},
 
-	EAST {
+	E {
 		public void print()
 		{
 			System.out.println("East");
 		}
 	},
 
-	SOUTH {
+	S {
 		public void print()
 		{
 			System.out.println("South");
 		}
 	},
 
-	WEST {
+
+	W {
 		public void print()
 		{
 			System.out.println("West");
 		}
+	};
+}
+
+
+public class Direction implements Component
+{
+	private Directions direction;
+
+	public static final float UNIT_DIAGONAL = 0.70710678118f;
+
+	public void print()
+	{
+	}
+
+	public static Vector2f getDirectionUnitVector(Direction d)
+	{
+		/*
+		switch (d) {
+		case N:
+			return new Vector2f(0.f, -1.f);
+		case E:
+			return new Vector2f(1.f, 0.f);
+		case W:
+			return new Vector2f(-1.f, 0.f);
+		case S:
+			return new Vector2f(0.f, 1.f);
+		default:
+			return new Vector2f(0, 0);
+		}*/
+
+		return new Vector2f(0, 0);
 	}
 }
