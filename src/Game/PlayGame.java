fdfba69ecb96509245cf6b79d8518d554090d698
jaredponn <jpon1234@gmail.com>
2019-02-16 02:55:23 -0700

    refactored the Sytems, moved them to smaller private functions. Camera centeringis in place

--- src/Game/PlayGame.java
+++ src/Game/PlayGame.java
@@ -3,36 +3,30 @@ package Game;
 import Resources.*;
 import Components.*;
 
-import poj.EngineState;
 import poj.Component.Components;
 import poj.Time.Timer;
+import poj.linear.Vector2f;
 
 import Game.Camera;
 
 import EntitySets.*;
 import TileMap.Map;
+
 import EntityTransforms.*;
 
-import java.awt.Color;
 import java.awt.event.KeyEvent;
 
 public class PlayGame extends World
 {
 	private Map map;
-	private Camera cam;    // camera
-	private Camera invCam; // inverse camera
+	private Camera entityCam; // camera
+	private Camera invCam;    // inverse camera
 
-	// TODO -- this should be moved somewhere in the map class. @Haiyang,
-	// can you please look into storing this state there? And also, the map
-	// tile width and heighgs are wrong
-	private static final float TILE_ROTATION = ((float)Math.PI / 4.f);
-	private static final float TILE_WIDTH = 64f;
-	private static final float TILE_HEIGHT = 32f;
+	private int player;
 
 	public PlayGame()
 	{
 		super();
-
 
 		// World loading
 		this.map = new Map(3);
@@ -43,13 +37,12 @@ public class PlayGame extends World
 		// this.map.addMapLayer(GameResources.mapLayer1);
 		// this.map.addMapLayer(GameResources.mapLayer2);
 
-		this.cam = new Camera();
+		this.entityCam = new Camera();
 
-		this.cam.setScalingForVector2(TILE_WIDTH, -TILE_HEIGHT);
-		this.cam.composeWithRotationForVector2XaxisCC(
-			PlayGame.TILE_ROTATION);
-		this.setCameraPosition(0f, 0f);
-
+		this.entityCam.setScalingForVector2(GameResources.TILE_WIDTH,
+						    -GameResources.TILE_HEIGHT);
+		this.entityCam.composeWithRotationForVector2XaxisCC(
+			GameResources.TILE_ROTATION);
 
 		this.invCam = new Camera();
 		this.updateInverseCamera();
@@ -57,18 +50,16 @@ public class PlayGame extends World
 
 	public void registerComponents()
 	{
-		// remember to register components
 		super.engineState.registerComponent(CollisionBody.class);
 		super.engineState.registerComponent(HasAnimation.class);
 		super.engineState.registerComponent(Render.class);
 		super.engineState.registerComponent(TileCord.class);
-		super.engineState.registerComponent(Speed.class);
 		super.engineState.registerComponent(WorldAttributes.class);
 		super.engineState.registerComponent(Direction.class);
+		super.engineState.registerComponent(Speed.class);
 	}
 	public void registerEntitySets()
 	{
-		// remember to register entity sets
 		super.engineState.registerSet(PlayerSet.class);
 		super.engineState.registerSet(MobSet.class);
 		super.engineState.registerSet(ConstructSet.class);
@@ -79,12 +70,12 @@ public class PlayGame extends World
 
 	{
 		// Player
-		super.engineState.spawnEntitySet(new PlayerSet());
+		this.player = super.engineState.spawnEntitySet(new PlayerSet());
+		super.engineState.spawnEntitySet(new MobSet());
 	}
 	public void clearWorld()
 	{
 	}
-
 
 	public void runGameLoop()
 	{
@@ -92,62 +83,21 @@ public class PlayGame extends World
 			super.setInitialTime();
 			this.processInputs();
 
+			// updating the camera
+
 			// SYSTEMS Go here
-			// enemyMovements / updates / path findings
-			// updatePositionFromVelocity() .....
-			// updateCameraPosition() .....
-
-			// updating the animation windows
-			for (HasAnimation a :
-			     super.engineState
-				     .getRawComponentArrayListPackedData(
-					     HasAnimation.class)) {
-				EntitySetTransforms.updateHasAnimationComponent(
-					a, this.dt);
-			}
-
-			// updating the cropping the render componets' sprite
-			// sheets
-			for (int i = super.engineState.getInitialComponentIndex(
-				     HasAnimation.class);
-			     Components.isValidEntity(i);
-			     i = super.engineState.getNextComponentIndex(
-				     HasAnimation.class, i)) {
-				EntitySetTransforms
-					.updateRenderComponentWindowFromHasAnimation(
-						super.engineState
-							.getComponentAt(
-								Render.class,
-								i),
-						super.engineState
-							.getComponentAt(
-								HasAnimation
-									.class,
-								i));
-			}
-
-			// updating the positions to screen coordinates
-			for (int i = super.engineState.getInitialComponentIndex(
-				     WorldAttributes.class);
-			     Components.isValidEntity(i);
-			     i = super.engineState.getNextComponentIndex(
-				     WorldAttributes.class, i)) {
-
-				EntitySetTransforms
-					.updateRenderScreenCoordinatesFromWorldCoordinates(
-						super.engineState
-							.getComponentAt(
-								WorldAttributes
-									.class,
-								i),
-						super.engineState
-							.getComponentAt(
-								Render.class,
-								i),
-						this.cam);
-			}
+			this.updateWorldAttribPositionFromDirectionAndSpeed(
+				this.dt);
+			this.centerCamerasPositionToPlayer();
+			this.updateAnimationWindows();
+			this.cropSpriteSheetsFromAnimationWindows();
+			this.updateRenderScreenCoordinatesFromWorldCoordinatesWithCamera();
 
 			this.render();
+
+			// updating the camera
+			this.centerCamerasPositionToPlayer();
+
 			this.updateInverseCamera();
 
 			super.setFinalTime();
@@ -161,112 +111,106 @@ public class PlayGame extends World
 	protected void processInputs()
 	{
 
-		// player manipulation
-		for (int player = super.engineState.getComponents()
-					  .getInitialSetIndex(PlayerSet.class);
-		     Components.isValidEntity(player);
-		     player = engineState.getComponents().getNextSetIndex(
-			     PlayerSet.class, player)) {
+		////// Movement Commands //////
 
-			if (super.inputPoller.isKeyDown(KeyEvent.VK_W)) {
-				System.out.println("w key is down");
-				super.engineState
-					.getComponentAt(WorldAttributes.class,
-							player)
-					.add(0, GameConfig.PLAYER_SPEED);
-			}
-
-			if (super.inputPoller.isKeyDown(KeyEvent.VK_D)) {
-				System.out.println("d key is down");
-				super.engineState
-					.getComponentAt(WorldAttributes.class,
-							player)
-					.add(GameConfig.PLAYER_SPEED, 0);
-			}
-			if (super.inputPoller.isKeyDown(KeyEvent.VK_S)) {
-				System.out.println("s key is down");
-				super.engineState
-					.getComponentAt(WorldAttributes.class,
-							player)
-					.add(0, -GameConfig.PLAYER_SPEED);
-			}
-			if (super.inputPoller.isKeyDown(KeyEvent.VK_A)) {
-				System.out.println("a key is down");
-				super.engineState
-					.getComponentAt(WorldAttributes.class,
-							player)
-					.add(-GameConfig.PLAYER_SPEED, 0);
-			}
-
-			////// Build Commands //////
-			if (super.inputPoller.isKeyDown(
-				    GameConfig.BUILD_TOWER)) {
-				System.out.print(
-					"q key is down. Should spawn tower at player location\n");
-				// TODO: get tile player is stood on
-				// TODO: highlight that tile?
-				// TODO: spawn new tower entity on tile
-				// TODO: make tower spawn on key up? (to prevent
-				// constant spawning if key down for more than 1
-				// frame)
-			}
-			if (super.inputPoller.isKeyDown(
-				    GameConfig.BUILD_TRAP)) {
-				System.out.print(
-					"e key is down. Should spawn trap at player location\n");
-				// TODO: get tile player is stood on
-				// TODO: highlight that tile?
-				// TODO: spawn new trap entity on tile
-				// TODO: make trap spawn on key up? (to prevent
-				// constant spawning if key down for more than 1
-				// frame)
-			}
-
-			////// Combat Commands //////
-			if (super.inputPoller.isKeyDown(
-				    GameConfig.ATTACK_KEY)) {
-				System.out.print(
-					"f key is down. Player character should be attacking\n");
-				// TODO: find adjacent tiles (and any enemies on
-				// them)
-				// TODO: apply damage to enemies
-				// TODO: attack on mouse click instead?
-			}
-			if (super.inputPoller.isLeftMouseButtonDown()) {
-				System.out.println(
-					"left mouse button is down. Player character should be attacking");
-				// TODO: find adjacent tiles (and any enemies on
-				// them)
-				// TODO: apply damage to enemies
-			}
-			if (super.inputPoller.isKeyDown(
-				    GameConfig.SWITCH_WEAPONS)) {
-				System.out.print(
-					"x key is down. Player character should be changing weapons\n");
-				// TODO: implement different weapons
-				// TODO: switch between weapons
-				// player.switchWeapon();
-			}
-
-			super.engineState
-				.getComponentAt(WorldAttributes.class, player)
-				.print();
-			System.out.println("x ="
-					   + super.inputPoller.getMouseX());
-			System.out.println("y ="
-					   + super.inputPoller.getMouseY());
+		if (super.inputPoller.isKeyDown(KeyEvent.VK_W)) {
+			System.out.println("w key is down");
+			super.getComponentAt(Direction.class, this.player)
+				.setDirection(CardinalDirections.N);
+			super.getComponentAt(Speed.class, this.player)
+				.setSpeed(GameConfig.PLAYER_SPEED);
+		} else if (super.inputPoller.isKeyDown(KeyEvent.VK_A)) {
+			System.out.println("a key is down");
+			super.getComponentAt(Direction.class, this.player)
+				.setDirection(CardinalDirections.E);
+			super.getComponentAt(Speed.class, this.player)
+				.setSpeed(GameConfig.PLAYER_SPEED);
+		} else if (super.inputPoller.isKeyDown(KeyEvent.VK_D)) {
+			System.out.println("d key is down");
+			super.getComponentAt(Direction.class, this.player)
+				.setDirection(CardinalDirections.W);
+			super.getComponentAt(Speed.class, this.player)
+				.setSpeed(GameConfig.PLAYER_SPEED);
+		} else if (super.inputPoller.isKeyDown(KeyEvent.VK_S)) {
+			System.out.println("s key is down");
+			super.getComponentAt(Direction.class, this.player)
+				.setDirection(CardinalDirections.S);
+			super.getComponentAt(Speed.class, this.player)
+				.setSpeed(GameConfig.PLAYER_SPEED);
+		} else // no movement key is pressed
+		{
+			super.getComponentAt(Speed.class, this.player)
+				.setSpeed(0);
 		}
+
+		////// Build Commands //////
+		if (super.inputPoller.isKeyDown(GameConfig.BUILD_TOWER)) {
+			System.out.print(
+				"q key is down. Should spawn tower at player location\n");
+			// TODO: get tile player is stood on
+			// TODO: highlight that tile?
+			// TODO: spawn new tower entity on tile
+			// TODO: make tower spawn on key up? (to prevent
+			// constant spawning if key down for more than 1
+			// frame)
+		}
+		if (super.inputPoller.isKeyDown(GameConfig.BUILD_TRAP)) {
+			System.out.print(
+				"e key is down. Should spawn trap at player location\n");
+			// TODO: get tile player is stood on
+			// TODO: highlight that tile?
+			// TODO: spawn new trap entity on tile
+			// TODO: make trap spawn on key up? (to prevent
+			// constant spawning if key down for more than 1
+			// frame)
+		}
+
+		////// Combat Commands //////
+		if (super.inputPoller.isKeyDown(GameConfig.ATTACK_KEY)) {
+			System.out.print(
+				"f key is down. Player character should be attacking\n");
+			// TODO: find adjacent tiles (and any enemies on
+			// them)
+			// TODO: apply damage to enemies
+			// TODO: attack on mouse click instead?
+		}
+		if (super.inputPoller.isLeftMouseButtonDown()) {
+			System.out.println(
+				"left mouse button is down. Player character should be attacking");
+			// TODO: find adjacent tiles (and any enemies on
+			// them)
+			// TODO: apply damage to enemies
+		}
+		if (super.inputPoller.isKeyDown(GameConfig.SWITCH_WEAPONS)) {
+			System.out.print(
+				"x key is down. Player character should be changing weapons\n");
+			// TODO: implement different weapons
+			// TODO: switch between weapons
+			// player.switchWeapon();
+		}
+
+		// super.getComponentAt(WorldAttributes.class, player).print();
+		// System.out.println("x =" + super.inputPoller.getMouseX());
+		// System.out.println("y =" + super.inputPoller.getMouseY());
 	}
 
 	protected void render()
 	{
-		map.renderTileMap(super.renderer);
+		for (Render r : this.map.getTileLayerRender(0)) {
+			// this if (r != null) was an awful api decision and
+			// needs to be removed unless there is a very very very
+			// good reason for why this is here, as it completely
+			// defeats the purpose of having a packed vector and a
+			// sparse vector
+			if (r != null)
+				Systems.pushRenderComponentToRenderer(
+					r, super.renderer);
+		}
 
-		for (Render r : super.engineState.getComponents()
-					.getRawComponentArrayListPackedData(
-						Render.class)) {
-			EntitySetTransforms.pushRenderComponentToRenderer(
-				r, super.renderer);
+		for (Render r :
+		     super.getRawComponentArrayListPackedData(Render.class)) {
+			Systems.pushRenderComponentToRenderer(r,
+							      super.renderer);
 		}
 
 		super.renderer.render();
@@ -275,14 +219,81 @@ public class PlayGame extends World
 
 	private void updateInverseCamera()
 	{
-		if (this.cam.isInvertible()) {
-			this.invCam =
-				new Camera((this.cam.unsafePureInverse()));
+		if (this.entityCam.isInvertible()) {
+			this.invCam = new Camera(
+				(this.entityCam.unsafePureInverse()));
 		}
 	}
 
-	private void setCameraPosition(float x, float y)
+
+	// TODO add the other camera for the tile map
+	private void centerCamerasPositionsToWorldAttribute(WorldAttributes n)
 	{
-		cam.setTranslationForVector2(x, y);
+		Vector2f tmp = n.getOriginCoord();
+
+		tmp.matrixMultiply(this.entityCam);
+
+		this.entityCam.setTranslationForVector2(
+			-tmp.x + super.windowWidth / 2f,
+			-tmp.y + super.windowHeight / 2f);
+	}
+
+	/// SYSTEMS
+	private void updateAnimationWindows()
+	{
+		for (HasAnimation a : super.getRawComponentArrayListPackedData(
+			     HasAnimation.class)) {
+			Systems.updateHasAnimationComponent(a, this.dt);
+		}
+	}
+
+	private void cropSpriteSheetsFromAnimationWindows()
+	{
+
+		for (int i = super.getInitialComponentIndex(HasAnimation.class);
+		     Components.isValidEntity(i);
+		     i = super.getNextComponentIndex(HasAnimation.class, i)) {
+			Systems.updateRenderComponentWindowFromHasAnimation(
+				super.getComponentAt(Render.class, i),
+				super.getComponentAt(HasAnimation.class, i));
+		}
+	}
+
+
+	private void
+	updateRenderScreenCoordinatesFromWorldCoordinatesWithCamera()
+	{
+
+		for (int i = super.getInitialComponentIndex(
+			     WorldAttributes.class);
+		     Components.isValidEntity(i);
+		     i = super.getNextComponentIndex(WorldAttributes.class,
+						     i)) {
+
+			Systems.updateRenderScreenCoordinatesFromWorldCoordinates(
+				super.getComponentAt(WorldAttributes.class, i),
+				super.getComponentAt(Render.class, i),
+				this.entityCam);
+		}
+	}
+
+	private void updateWorldAttribPositionFromDirectionAndSpeed(float dt)
+	{
+		for (int i = super.getInitialComponentIndex(Direction.class);
+		     Components.isValidEntity(i);
+		     i = super.getNextComponentIndex(Direction.class, i)) {
+			Vector2f tmp = Systems.getVelocityFromDirectionAndSpeed(
+				getComponentAt(Direction.class, i),
+				getComponentAt(Speed.class, i));
+			tmp.mul(dt);
+			getComponentAt(WorldAttributes.class, i).add(tmp);
+		}
+	}
+
+	private void centerCamerasPositionToPlayer()
+	{
+		this.centerCamerasPositionsToWorldAttribute(
+			engineState.getComponentAt(WorldAttributes.class,
+						   this.player));
 	}
 }
