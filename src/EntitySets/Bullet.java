adb60bbdfa20e3dd839eb7512ad2b2cb8850e154
jaredponn <jpon1234@gmail.com>
2019-02-16 19:48:12 -0700

    bullet

--- src/EntitySets/Bullet.java
+++ src/EntitySets/Bullet.java
@@ -1,14 +1,20 @@
 package EntitySets;
 
-import Components.*;    //include this
-import poj.EntitySet.*; //include this
+import poj.EntitySet.EntitySet;
+import Components.*;
+
+import Components.Render;
+
 import Resources.GameConfig;
+import Resources.GameResources;
+
 
 public class Bullet extends EntitySet
 {
 	public Bullet()
 	{
 		super();
+		addComponent(new Render(GameResources.bulletImage));
 		addComponent(new WorldAttributes(GameConfig.BULLET_WIDTH,
 						 GameConfig.BULLET_HEIGHT));
 		addComponent(new Speed(GameConfig.BULLET_SPEED));
