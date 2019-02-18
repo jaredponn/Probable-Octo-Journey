0db072caa850cd04745db026358f04677ab2b157
chinahehaiyang <imhhy123@hotmail.com>
2019-02-17 20:16:57 -0700

    changed map states

--- src/EntitySets/MobSet.java
+++ src/EntitySets/MobSet.java
@@ -14,16 +14,22 @@ public class MobSet extends EntitySet
 	{
 		super();
 
-		addComponent(new Movement(GameConfig.MOB_VELOCITY));
-
 		addComponent(new Render(new ImageRenderObject(
-			0, 0, GameResources.playerSpriteSheet)));
-		// TODO: mob sprite sheet
-
+			0, 0, GameResources.enemySpriteSheet)));
 		addComponent(new WorldAttributes(new Vector2f(0, 0),
 						 GameConfig.MOB_WIDTH,
 						 GameConfig.MOB_HEIGHT));
 
+		addComponent(
+			new HasAnimation(GameResources.enemyNMoveAnimation));
+		addComponent(new Movement(GameConfig.MOB_VELOCITY));
+		addComponent(new MovementDirection(CardinalDirections.N));
+		addComponent(new FacingDirection(CardinalDirections.N));
+
+		// TODO: mob sprite sheet
+
+
+		//
 		// TODO: make mobs a different size than the player?
 
 		// TODO: mob animations (walking, idle, attacking, dying)
