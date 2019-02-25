d116e770309b625b8df18d2e90d63ec9bb5da5f2
chinahehaiyang <imhhy123@hotmail.com>
2019-02-25 16:33:54 -0700

    put comments in tileMap and mapGeneration

--- src/TileMap/MapLayer.java
+++ src/TileMap/MapLayer.java
@@ -3,6 +3,13 @@ package TileMap;
 
 public class MapLayer extends poj.EngineState
 {
+	/**
+	 * Constructs the MapLayer class, which is a nice wrapper for using a
+	 * specific map layer
+	 *
+	 * @param  CAPACITY	creates the map
+	 *  @return      void
+	 */
 	public MapLayer(int CAPACITY)
 	{
 		super(CAPACITY);
