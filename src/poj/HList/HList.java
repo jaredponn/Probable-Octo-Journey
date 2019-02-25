827906a3014d0345bf72744d11f2f20c1910b340
Jared Pon <jpon1234@gmail.com>
2019-02-25 01:18:09 -0700

    Update HList.java

--- src/poj/HList/HList.java
+++ src/poj/HList/HList.java
@@ -1,7 +1,8 @@
 package poj.HList;
 
 import poj.HList.HCons;
-
+// Parts of the code was from: ://apocalisp.wordpress.com/2008/10/23/heterogeneous-lists-and-the-limits-of-the-java-type-system/
+// And Sandy Macgire's Thinking with TypesTypes
 public abstract class HList<T extends HList<T>>
 {
 	public static final <U, Us extends HList<Us>> HCons<U, Us> hcons(U n,
