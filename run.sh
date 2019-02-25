0cc80a3ad1fe46f224091a28509c2f5958eddea9
jaredponn <jpon1234@gmail.com>
2019-02-24 20:53:53 -0700

    collisions coming together and new run.sh file that will run the project

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p bin
+
+javac -d bin/ src/*.java src/Components/*.java src/EntitySets/*.java src/Game/*.java src/poj/*.java src/poj/Collisions/*.java src/poj/Component/*.java src/poj/EntitySet/*.java src/poj/GameWindow/*.java src/poj/HList/*.java src/poj/linear/*.java src/poj/Logger/*.java src/poj/Render/*.java src/poj/Time/*.java src/Resources/*.java src/TileMap/*.java && java -cp bin/ Main
--- src/poj/Collisions/GJK.java
+++ src/poj/Collisions/GJK.java
@@ -2,6 +2,7 @@ package poj.Collisions;
 
 import poj.linear.*;
 import java.util.ArrayList;
+import poj.Logger.*;
 
 public class GJK
 {
@@ -18,14 +19,15 @@ public class GJK
 	public static boolean areColliding(CollisionShape cola,
 					   CollisionShape colb)
 	{
-		ArrayList<Vector2f> supportSet = new ArrayList();
+		ArrayList<Vector2f> simplex = new ArrayList<Vector2f>();
 
-		Vector2f d = new Vector2f(1, 0); // intial serach direction
+		Vector2f s = GJK.support(cola, colb, new Vector2f(1, 0));
 
-		supportSet.add(
-			GJK.support(cola, colb, d)); // first boundary added
+		simplex.add(s); // first boundary added in a
+				// random riection
 
-		d.negate(); // change direction
+		Vector2f d =
+			s.pureNegate(); // change direction to the opposoite
 
 		while (true) {
 			Vector2f a = GJK.support(cola, colb, d); // new support
@@ -34,17 +36,53 @@ public class GJK
 			{
 			}
 
-			// else if it does cross the origin
-			supportSet.add(a);
+			// else if it does cross the origin add it i th simplex
+			simplex.add(a);
 
-			if (doSimplex(supportSet, d)) // intersection
+			if (doSimplex(simplex, d)) // intersection
 			{
 			}
 		}
 	}
 
-	public static boolean doSimplex(ArrayList<Vector2f> s, Vector2f d)
+	// determiens the  closest point to the origin
+	public static boolean doSimplex(ArrayList<Vector2f> simplex, Vector2f d)
 	{
+		switch (simplex.size()) {
+		case 2:
+			// simplex: [b, a]
+			Vector2f a = simplex.get(1); // point just added
+			Vector2f b = simplex.get(0);
+
+			Vector2f ab = b.pureSubtract(a);
+			Vector2f ao = a.pureNegate();
+
+
+			Vector2f newsearchdirecion;
+
+			if (Vector2f.dot(ab, ao) > 0) // if ab is the simplex
+			{
+				// ab is the simplex
+				// the new normal perpendicular -- this is the
+				// new search direction
+				newsearchdirecion =
+					Vector2f.pureTripleProduct(ab, ao, ab);
+			} else {
+
+				newsearchdirecion = ao;
+			}
+
+
+		case 3:
+
+			// simplex: [c, b, a]
+
+		default:
+			Logger.lassert(
+				"ERROR in GJK -- simplexes of size 2 and 3 are only supported");
+			break;
+		}
+
 		return true;
 	}
 }
