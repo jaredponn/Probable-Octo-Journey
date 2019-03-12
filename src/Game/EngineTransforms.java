9c268235907e6ef55e89d3cbd9dcad3bca0344c2
chinahehaiyang <imhhy123@hotmail.com>
2019-03-11 20:32:13 -0600

    made zombie move in the previous direction when it hits the player ( will change to attack animation later)

--- src/Game/EngineTransforms.java
+++ src/Game/EngineTransforms.java
@@ -237,19 +237,22 @@ public class EngineTransforms
 			engineState.getComponentAt(Movement.class, mob1)
 				.setSpeed(0);
 
-			CardinalDirections tempDir =
-				engineState
-					.getComponentAt(MovementDirection.class,
-							mob1)
-					.getDirection();
+			/*
+		CardinalDirections tempDir =
+			engineState
+				.getComponentAt(MovementDirection.class,
+						mob1)
+				.getDirection();
 
-			// idle position
-			engineState.getComponentAt(HasAnimation.class, mob1)
-				.setAnimation(
-					findEnemyFacingSprite(tempDir, 0));
+		// idle position
+		engineState.getComponentAt(HasAnimation.class, mob1)
+			.setAnimation(
+				findEnemyFacingSprite(tempDir, 0));
+				*/
 			return;
 		}
 
+		// in the same world cord
 		if ((int)mobPosition.x == (int)playerPosition.x
 		    && (int)mobPosition.y == (int)playerPosition.y) {
 			// TODO: NEED TO INTEGRATE THIS WITH COLLISION!!
@@ -305,6 +308,7 @@ public class EngineTransforms
 			engineState.getComponentAt(Movement.class, mob1)
 				.setSpeed(0f);
 			// zombie will be in idle
+
 			CardinalDirections tempDir =
 				engineState
 					.getComponentAt(MovementDirection.class,
