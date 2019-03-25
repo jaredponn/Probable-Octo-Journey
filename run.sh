e7a3b9371df0ecb53d33fedce7ffc708c0401bb4
jaredponn <jpon1234@gmail.com>
2019-03-25 04:01:02 -0600

    should be fixed to work with everything again

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p rbin
+touch scores.txt
 
 javac -d rbin/ src/*.java src/Components/*.java src/EntitySets/*.java src/Game/*.java src/poj/*.java src/App/*.java src/poj/Collisions/*.java src/poj/Component/*.java src/poj/EntitySet/*.java src/poj/GameWindow/*.java src/poj/HList/*.java src/poj/linear/*.java src/poj/Logger/*.java src/poj/Render/*.java src/poj/Time/*.java src/Resources/*.java src/TileMap/*.java src/PathFinding/*.java  && java -cp rbin/ Main
