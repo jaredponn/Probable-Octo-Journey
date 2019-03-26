e15a7847bae6f0e64ee6aecfa784ec194f2bd316
chinahehaiyang <imhhy123@hotmail.com>
2019-03-25 20:46:27 -0600

    decreased steering behaviour

--- src/Game/PlayGame.java
+++ src/Game/PlayGame.java
@@ -416,7 +416,7 @@ public class PlayGame extends World
 			this.engineState, PlayerSet.class);
 		EngineTransforms
 			.steerMovementVelocityFromMovementDirectionForSet(
-				this.engineState, MobSet.class, 1 / 32f);
+				this.engineState, MobSet.class, 1 / 29f);
 	}
 
 	protected Vector2f getPositionToMouseDelta(Vector2f v)
