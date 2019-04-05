7e3827a8ead5568827d2915ba66b671b69a05131
jaredponn <jpon1234@gmail.com>
2019-04-04 22:39:33 -0600

    all collision boxes were moved to the first layer so it removes the need of iteratin through everything

--- src/Game/CombatFunctions.java
+++ src/Game/CombatFunctions.java
@@ -69,11 +69,8 @@ public class CombatFunctions
 	public static void bulletHitHandler(PlayGame g, int bullet)
 	{
 		EngineState mainState = g.getEngineState();
-		EngineState mapState0 = g.getMap().getLayerEngineState(0);
-		EngineState mapState1 = g.getMap().getLayerEngineState(1);
-		EngineState mapState2 = g.getMap().getLayerEngineState(2);
-		EngineState mapState3 = g.getMap().getLayerEngineState(3);
-		EngineState mapState4 = g.getMap().getLayerEngineState(4);
+		EngineState mapState0 = g.getMap().getLayerEngineState(
+			g.getMap().COLLISION_LAYER);
 		// map layer 0: ???
 		//			 1: map edge and small rocks
 		//			 2: cars and buildings
@@ -124,25 +121,7 @@ public class CombatFunctions
 
 		// check for bullet collision with wall
 		for (PhysicsPCollisionBody wall :
-		     mapState1.getRawComponentArrayListPackedData(
-			     PhysicsPCollisionBody.class)) {
-			if (Systems.arePCollisionBodiesColliding(
-				    gjk, bulletBody, wall)) {
-				removeBullet(mainState, bullet);
-				return;
-			}
-		}
-		for (PhysicsPCollisionBody wall :
-		     mapState2.getRawComponentArrayListPackedData(
-			     PhysicsPCollisionBody.class)) {
-			if (Systems.arePCollisionBodiesColliding(
-				    gjk, bulletBody, wall)) {
-				removeBullet(mainState, bullet);
-				return;
-			}
-		}
-		for (PhysicsPCollisionBody wall :
-		     mapState3.getRawComponentArrayListPackedData(
+		     mapState0.getRawComponentArrayListPackedData(
 			     PhysicsPCollisionBody.class)) {
 			if (Systems.arePCollisionBodiesColliding(
 				    gjk, bulletBody, wall)) {
@@ -395,7 +374,8 @@ public class CombatFunctions
 				shellSpeed));
 
 		// decrease ammo
-		engineState.unsafeGetComponentAt(Ammo.class, turret).decreaseAmmo(1);
+		engineState.unsafeGetComponentAt(Ammo.class, turret)
+			.decreaseAmmo(1);
 
 		// destroy turret if out of ammo
 		if (engineState.unsafeGetComponentAt(Ammo.class, turret)
