9ae0a77f1974ba46ed913c91d13259154391c9fe
Alex Stark <xelakrats@gmail.com>
2019-04-08 08:18:33 -0600

    can enter initials on game over screen

--- src/Game/GameOver.java
+++ src/Game/GameOver.java
@@ -1,12 +1,5 @@
 package Game;
 
-/**
- * GameOver game state. UNUSED AND UNIMPLEMENTED
- * Date: March 10, 2019
- * @author Jared
- * @version 1.0
- */
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
@@ -35,11 +28,15 @@ import java.io.*;
 import java.util.*;
 import java.util.ArrayList;
 
-
+/**
+ * GameOver game state.
+ * @author Jared and Alex
+ * @version 2.0 - 04/03/19
+ */
 public class GameOver extends World
 {
 	protected int newScore;
-	protected ArrayList<Integer> scores;
+	protected ArrayList<ScoreTuple> scores = new ArrayList<ScoreTuple>();
 	protected boolean isHighScore;
 
 	protected Queue<RenderObject> renderBuffer;
@@ -48,6 +45,20 @@ public class GameOver extends World
 	protected static final int FONT_SIZE = 32;
 	protected static final Font FONT =
 		new Font("TimesRoman", Font.BOLD, FONT_SIZE);
+	
+	protected ArrayList<Integer> initials = new ArrayList<Integer>() {
+		{
+			add(65);
+			add(65);
+			add(65);
+		}
+	};
+	protected int currentInitial = 0;
+	protected StringRenderObject initialRender = new StringRenderObject(
+				initialsToString(),30,30,Color.DARK_GRAY,FONT);
+	
+	protected Scanner is = null;
+	
 
 	public GameOver(int width, int height, Renderer renderer,
 			InputPoller inputPoller, int newScore)
@@ -55,40 +66,51 @@ public class GameOver extends World
 		super(width, height, renderer, inputPoller);
 		this.newScore = newScore;
 		this.renderBuffer = new LinkedList<RenderObject>();
-
-		this.scores = new ArrayList<Integer>();
-
-		Scanner is = null;
+		
 		try {
 			is = new Scanner(new File(SCORES_FILE_NAME));
+			is.close();
 		} catch (FileNotFoundException e) {
-			Logger.lassert("error while opening file "
-				       + SCORES_FILE_NAME);
+			System.out.println("Cannot find file: " + SCORES_FILE_NAME);
+			try {
+				PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(SCORES_FILE_NAME)));
+				writer.write(""+ 0 + "\nAAA");
+				for ( int i = 0 ; i < 9 ; i++ ) {
+	            	writer.write("\n"+ 0+"\nAAA");
+	            }
+	            writer.close();
+			}
+			catch (IOException ioe) {
+				Logger.lassert("Cannot create file: "
+					       + SCORES_FILE_NAME);
+			}
 		}
-
-		while (is.hasNextLine()) {
-			String line = is.nextLine();
-			scores.add(Integer.parseInt(line));
+		
+		try {
+			is = new Scanner(new File(SCORES_FILE_NAME));
+			String thisName = "";
+			int thisScore = 0;
+			while (is.hasNextLine()) {
+				thisScore = Integer.parseInt(is.nextLine());
+				thisName = is.nextLine();
+				scores.add(new ScoreTuple(thisScore , thisName));
+			}
+			is.close();
 		}
-
+		catch (IOException ioe) {
+			Logger.lassert("Cannot load high score info");
+		}
+		
 		this.isHighScore = true;
-		for (Integer i : scores) {
-			if (this.newScore <= i) {
+		for (ScoreTuple t : scores) {
+			if (this.newScore <= t.getScore()) {
 				this.isHighScore = false;
 				break;
 			}
 		}
-
-		scores.add(newScore);
-
-		for (Integer i : scores) {
-			System.out.println(i);
-		}
-
-		Collections.sort(this.scores);
-		Collections.reverse(this.scores);
-
-		overWriteScoresTextFile();
+		
+		Collections.sort(scores);
+		Collections.reverse(scores);
 	}
 
 	public void runGame()
@@ -123,7 +145,7 @@ public class GameOver extends World
 
 		for (int i = 0; i < 5 && i < scores.size(); ++i) {
 			renderBuffer.add(new StringRenderObject(
-				(i + 1) + ": " + scores.get(i),
+				scores.get(i).getName() +": " + scores.get(i).getScore(),
 				super.windowWidth / 2 - 20,
 				(i + 10) * FONT_SIZE, Color.darkGray, FONT));
 		}
@@ -134,6 +156,8 @@ public class GameOver extends World
 			super.windowHeight - FONT_SIZE - 20, Color.darkGray,
 			FONT));
 
+		renderBuffer.add(initialRender);
+
 		this.processInputs();
 	}
 
@@ -141,6 +165,37 @@ public class GameOver extends World
 	{
 		if (inputPoller.isKeyDown(KeyEvent.VK_ENTER))
 			quit();
+		
+		// Choose letter
+		if (inputPoller.isKeyDown(KeyEvent.VK_UP) ) {
+			if (initials.get(currentInitial) < 90) {
+				Integer initial = initials.get(currentInitial);
+				initials.set(currentInitial, initial + 1);
+			}
+			else {
+				initials.set(currentInitial, 65);
+			}
+			initialRender.setStr(initialsToString());
+		}
+		if (inputPoller.isKeyDown(KeyEvent.VK_DOWN) ) {
+			if (initials.get(currentInitial) > 65) {
+				Integer initial = initials.get(currentInitial);
+				initials.set(currentInitial, initial - 1);
+			}
+			else {
+				initials.set(currentInitial , 90 );
+			}
+			initialRender.setStr(initialsToString());
+		}
+		// Choose slot
+		if (inputPoller.isKeyDown(KeyEvent.VK_LEFT)) {
+			if (currentInitial > 0)
+				currentInitial--;
+		}
+		if (inputPoller.isKeyDown(KeyEvent.VK_RIGHT)) {
+			if (currentInitial < 2)
+				currentInitial++;
+		}
 	}
 
 
@@ -155,8 +210,11 @@ public class GameOver extends World
 			FileWriter fw =
 				new FileWriter(new File(SCORES_FILE_NAME));
 			String tmp = "";
-			for (Integer i : this.scores) {
-				tmp += i + "\n";
+			for (int i = 0 ; i < scores.size() ; i ++) {
+				if (i != 0)
+					tmp += "\n" + scores.get(i).getScore() + "\n" + scores.get(i).getName();
+				else
+					tmp += scores.get(i).getScore() + "\n" + scores.get(i).getName();
 			}
 			fw.write(tmp);
 			fw.close();
@@ -166,5 +224,61 @@ public class GameOver extends World
 				"Error overwriting scores text file.");
 			e.printStackTrace();
 		}
+	}
+	
+	protected String initialsToString() {
+		String stringOfInitials = "";
+		for (int i : initials) {
+			String thisInitial = Character.toString((char) i);
+			stringOfInitials = stringOfInitials + thisInitial;
+		}
+		return stringOfInitials;
+	}
+	
+	public void quit() {
+		
+		scores.add(new ScoreTuple(newScore,initialsToString()));
+
+		Collections.sort(scores);
+		Collections.reverse(scores);
+		
+		while (scores.size() > 10) {
+			scores.remove(10);
+		}
+
+		overWriteScoresTextFile();
+		super.quit();
+	}
+	
+	private class ScoreTuple implements Comparable<ScoreTuple>{
+		 private String name;
+		 private int score;
+		 
+		 public ScoreTuple( int defScore , String defName ) {
+			 name = defName;
+			 score = defScore;
+		 }
+		 
+		 public String getName() {
+			 return name;
+		 }
+		 
+		 public int getScore() {
+			 return score;
+		 }
+		 
+		 public void print() {
+			 System.out.println("Name: "+name + " | Score: "+score);
+		 }
+		 
+		 @Override
+		 public int compareTo(ScoreTuple o) {
+			 if (this.getScore() > o.getScore() )
+				 return 1;
+			 else if (this.getScore() < o.getScore() )
+				 return -1;
+			 else
+				 return 0;
+		 }
 	}
 }
