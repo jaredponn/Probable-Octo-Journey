c9bfac396e1f5f398430147c0faabe75b5f9b0cc
jaredponn <jpon1234@gmail.com>
2019-04-09 00:18:28 -0600

    clang format

--- src/Components/Damage.java
+++ src/Components/Damage.java
@@ -7,47 +7,61 @@ import poj.Component.Component;
  * @author Alex
  * @version 1.0
  */
-public class Damage implements Component{
-	
+public class Damage implements Component
+{
+
 	private int damage;
-	
+
+	/**
+	 * initialize the amount of damage this entity deals
+	 */
+	public Damage()
+	{
+	}
+
 	/**
 	 * initialize the amount of damage this entity deals
 	 * @param dmg: the amount of damage this deals
 	 */
-	public Damage (int dmg ) {
+	public Damage(int dmg)
+	{
 		this.damage = dmg;
 	}
-	
+
+
 	/**
 	 * get the amount of damage this entity deals
 	 * @return the amount of damage this entity deals
 	 */
-	public int getDamage() {
+	public int getDamage()
+	{
 		return this.damage;
 	}
-	
+
 	/**
 	 * set the amount of damage this entity deals
 	 * @param dmg: the new amount of damage this entity will deal
 	 */
-	public void setDamage( int dmg ) {
+	public void setDamage(int dmg)
+	{
 		this.damage = dmg;
 	}
-	
+
 	/**
 	 * increase/decrease the amount of damage this entity deals
 	 * @param mod: the amount to increase/decrease damage by
 	 */
-	public void changeDamage( int mod ) {
+	public void changeDamage(int mod)
+	{
 		this.damage += mod;
 	}
-	
+
 	/**
 	 * prints the amount of damage this entity deals
 	 */
-	public void print() {
-		System.out.println("This entity deals " +this.damage+ " damage.");
+	public void print()
+	{
+		System.out.println("This entity deals " + this.damage
+				   + " damage.");
 	}
-
 }
