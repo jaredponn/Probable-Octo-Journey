bb53593bcfad91a05ac440acc2aee11cc95a8f91
Alex Stark <xelakrats@gmail.com>
2019-04-12 21:22:35 -0600

    components commented

--- src/Components/Movement.java
+++ src/Components/Movement.java
@@ -17,22 +17,38 @@ public class Movement implements Component
 	private float speed;
 	private Vector2f vel;
 
+	/**
+	 * Set speed and create a new velocity vector
+	 * @param n: the speed to set this entity to
+	 */
 	public Movement(float n)
 	{
 		this.speed = n;
 		this.vel = new Vector2f(0f, 0f);
 	}
 
+	/**
+	 * Get the current speed of this entity
+	 * @return the current speed as a float
+	 */
 	public float getSpeed()
 	{
 		return this.speed;
 	}
 
+	/**
+	 * Set the speed of this entity
+	 * @param n: the float to set the speed to
+	 */
 	public void setSpeed(float n)
 	{
 		this.speed = n;
 	}
 
+	/**
+	 * Get the current velocity vector of this entity
+	 * @return the current velocity vector
+	 */
 	public Vector2f getVelocity()
 	{
 		return this.vel;
@@ -43,17 +59,27 @@ public class Movement implements Component
 		return this.vel.pureMul((float)dt);
 	}
 
+	/**
+	 * Set the velocity vector from another vector
+	 * @param n: the vector to copy
+	 */
 	public void setVelocity(Vector2f n)
 	{
 		setVelocity(n.x, n.y);
 	}
 
+	/**
+	 * Set the velocity from a pair of floats
+	 */
 	public void setVelocity(float x, float y)
 	{
 		this.vel.x = x;
 		this.vel.y = y;
 	}
 
+	/**
+	 * Print the current components of this vector to console
+	 */
 	public void print()
 	{
 		System.out.println("Speed Component: speed = " + this.speed
